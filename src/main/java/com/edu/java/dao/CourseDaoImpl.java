b433d23f57ac07a8100f5f32829adfae1480dfd5
YJ <yedoong919@gmail.com>
2022-03-16 18:06:22 +0900

    user_교육 목록만 볼 수 있도록 수정

--- src/main/java/com/edu/java/dao/CourseDaoImpl.java
+++ src/main/java/com/edu/java/dao/CourseDaoImpl.java
@@ -1,26 +1,44 @@
 package com.edu.java.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
 import com.edu.java.dto.CourseDto;
+import com.edu.java.dto.FaqDto;
 
 @Repository
 public class CourseDaoImpl implements CourseDao{
 	@Inject
-	SqlSession sqlsession;
+	SqlSession sqlSession;
+	
+	// LIST
+	@Override
+	public List<CourseDto> courseList() {
+		List<CourseDto> courseList = new ArrayList<CourseDto>();
+		
+		try {
+			courseList = sqlSession.selectList(NAMESPACE + "courseList");
+		} catch (Exception e) {
+			System.out.println("[error] : course list");
+			e.printStackTrace();
+		}
+		return courseList;
+	}
 	
 	// 교육 등록(insert)
 	@Override
 	public void courseInsertRes(CourseDto dto) throws Exception{
-		sqlsession.insert(NAMESPACE + "courseInsertRes", dto);
+		sqlSession.insert(NAMESPACE + "courseInsertRes", dto);
 	}
 	
 	// 교육 수정
 	@Override
 	public void courseUpdateRes(CourseDto dto) throws Exception{
-		sqlsession.update(NAMESPACE + "courseUpdateRes", dto);
+		sqlSession.update(NAMESPACE + "courseUpdateRes", dto);
 	}
 }
