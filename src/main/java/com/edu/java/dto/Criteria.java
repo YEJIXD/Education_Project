33d3ba8a564284a137965c6cdbc997bba239ded2
YJ <yedoong919@gmail.com>
2022-03-23 18:23:28 +0900

    페이지 추가/삭제 및 adminCourseList 페이징 관련 css 추가

--- src/main/java/com/edu/java/dto/Criteria.java
+++ src/main/java/com/edu/java/dto/Criteria.java
@@ -1,36 +1,15 @@
 package com.edu.java.dto;
 
-import org.springframework.stereotype.Component;
-
-import lombok.Data;
-
-@Data
 public class Criteria {
 	private int page;			// 현재 페이지
 	private int amount;			// 한 페이지 당 보여질 게시물 갯수 (perPageNum)
-	private int skip;			// 스킵할 게시물 수 ( (pageNum -1) * amount )
+	private int skip;			// 스킵할 게시물 수 ( (page -1) * amount )
 	
 	
 	// 기본 생성자 => 기본 세팅 : pageNum = 1, amount = 10
 	public Criteria() {
 		this(1,10);
 		this.skip = 0;
-	}
-	
-	public void setPage(int page) {
-		if(page <= 0) {
-			this.page = 1;
-			return;
-		}
-		this.page = page;
-	}
-	
-	public void setAmout(int amount) {
-		if(amount <= 0 || amount > 100) {
-			this.amount = 10;
-			return;
-		}
-		this.amount = amount;
 	}
 	
 	// 생성자 => 원하는 pageNum, 원하는 amount
@@ -40,6 +19,54 @@ public class Criteria {
 		this.skip = (page-1)*amount;
 	}
 	
+	public int getPage() {
+		return page;
+	}
 
+//	public void setPage(int page) {
+//		this.skip= (page-1)*this.amount;
+//		this.page = page;
+//	}
+	
+	public void setPage(int page) { 
+		if(page <= 0) { 		// 페이지가 음수 값이 되지 않도록 설정 => 음수 값이 되면 1페이지를 나타낸다.
+			this.page = 1; 
+			return; 
+	 	}
+	 		this.page = page; 	
+	 	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.skip= (this.page-1)*amount;
+		this.amount = amount;
+	}
+	
+	/*
+	 * public void setPerPageNum(int pageCount) {
+	        int cnt = this.perPageNum;
+	        if(pageCount != cnt) {
+	            this.perPageNum = cnt;
+	        } else {
+	            this.perPageNum = pageCount;
+	        }
+    	}
+	 * */
+
+	public int getSkip() {
+		return skip;
+	}
+
+	public void setSkip(int skip) {
+		this.skip = skip;
+	}
+	
+	/*
+	 * public void setAmount(int amount) { if(amount <= 0 || amount > 100) {
+	 * this.amount = 10; return; } this.amount = amount; }
+	 */
 	
 }
---
+++ src/main/java/com/edu/java/dto/PageDto.java
@@ -0,0 +1,94 @@
+package com.edu.java.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class PageDto{
+	// Search
+	private String searchType;		// 검색 타입
+	private String keyword = "";			// 키워드
+
+	// Paging
+	private int startPage;		// 시작 페이지	
+	private int endPage = 10;		// 끝 페이지
+	private boolean prev, next;	// 이전, 다음 페이지 유무
+	private int total;			// 전체 게시글 수
+	private Criteria cri;		// Criteria 객체 (현재 페이지, 페이지당 게시물 표시수 정보 => page(현재 페이지) 변수 값을 얻기 위해 선언)
+	private int displayPageNum = 10;	// 화면 하단에 보여지는 페이지 버튼 수
+	
+	public PageDto() {
+		super();
+	}
+	
+	public PageDto(int startPage, int endPage, boolean prev, boolean next, String searchType, String keyword, int total,
+			Criteria cri, int displayPageNum) {
+		super();
+		this.startPage = startPage;
+		this.endPage = endPage;
+		this.prev = prev;
+		this.next = next;
+		this.searchType = searchType;
+		this.keyword = keyword;
+		this.total = total;
+		this.cri = cri;
+		this.displayPageNum = displayPageNum;
+	}
+	
+	public PageDto(Criteria cri, int total) {
+		this.cri = cri;
+		this.total = total;
+		
+		this.endPage = (int)(Math.ceil(cri.getPage() / 10.0)) * 10;
+		
+		this.startPage = this.endPage - 9;
+		
+		// 실제 끝 페이지
+		int realEnd = (int)(Math.ceil((total * 1.0)) / cri.getAmount());
+		
+		// 실제 끝 페이지(realEnd)가 끝 페이지(endPage)보다 작으면 endPage를 realEnd페이지 값으로 변경 
+		if(realEnd <= this.endPage) {
+			this.endPage = realEnd;
+		}
+		
+		// 시작 페이지가 1페이지보다 크면 prev가 true
+		this.prev = this.startPage > 1;
+		// 끝 페이지가 실제 끝페이지(realEnd)보다 작으면 next가 true
+		this.next = this.endPage < realEnd;
+	}
+	
+	public void setCriteria(Criteria cri) { 
+		this.cri = cri; 
+	} 
+	
+	public void setTotalCount(int total) { 
+		this.total = total; 
+		calcData();			// 총 게시글 수를 세팅할 때 calcData()메서드를 호출하여 페이징 관련 버튼 계산을 함
+	}
+	
+	// 게시글의 전체 갯수가 결정되면 calcData 메소드를 호출하여 계산 실행 => 페이징 버튼들을 생성하는 계산식 작성
+	private void calcData() { 
+		//끝 페이지 번호 = (현재 페이지 번호 / 화면에 보여질 페이지 번호의 갯수) * 화면에 보여질 페이지 번호의 갯수
+		endPage = (int)(Math.ceil(cri.getPage() / (double) displayPageNum) * displayPageNum); 
+
+		//시작 페이지 번호 = (끝 페이지 번호 - 화면에 보여질 페이지 번호의 갯수) + 1
+		startPage = (endPage - displayPageNum) + 1; 
+		
+		//마지막 페이지 번호 = 총 게시글 수 / 한 페이지당 보여줄 게시글의 갯수
+		int tempEndPage = (int)(Math.ceil(total / (double) cri.getAmount())); 
+		
+		if (endPage > tempEndPage) {
+			endPage = tempEndPage; 
+		} 
+		
+		//이전 버튼 생성 여부 = 시작 페이지 번호 == 1 ? false : true
+		prev = startPage == 1 ? false : true; 
+		
+		//다음 버튼 생성 여부 = 끝 페이지 번호 * 한 페이지당 보여줄 게시글의 갯수 < 총 게시글의 수 ? true: false
+		next = endPage * cri.getAmount() >= total ? false : true; 
+	}
+
+}
