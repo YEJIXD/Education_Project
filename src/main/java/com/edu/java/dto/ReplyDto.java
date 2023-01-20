e81339c8ae0106a92f9eb8d866ae4ce3d3c6f0be
YEJI <yedoong919@gmail.com>
2023-01-20 23:59:11 +0900

    0120_replyDto 추가 (게시글과 분리)

---
+++ src/main/java/com/edu/java/dto/ReplyDto.java
@@ -0,0 +1,11 @@
+package com.edu.java.dto;
+
+import java.util.Date;
+
+public class ReplyDto {
+	private int re_no;		
+	private String re_title;	
+	private String re_content;
+	private Date input_date;	
+	private Date upd_date;	
+}
