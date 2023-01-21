764abac17cbf0350b15ff74e6cf6660744c68e18
YEJI <yedoong919@gmail.com>
2023-01-21 23:41:05 +0900

    0120 _ Reply 테이블 컬럼 추가

--- src/main/java/com/edu/java/dto/ReplyDto.java
+++ src/main/java/com/edu/java/dto/ReplyDto.java
@@ -3,9 +3,12 @@ package com.edu.java.dto;
 import java.util.Date;
 
 public class ReplyDto {
-	private int re_no;		
-	private String re_title;	
-	private String re_content;
+	
+	private MemberDto mDto;
+	
+	private int seq;		
+	private String content;
+	private String writer;
 	private Date input_date;	
 	private Date upd_date;	
 }
