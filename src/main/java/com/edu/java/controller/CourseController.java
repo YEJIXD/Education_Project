04f8471f2c81b7a91b3fea23f89d77223097d7ee
YEJI <yedoong919@gmail.com>
2023-01-22 23:05:11 +0900

    0122 _ controller URI GET/POST 두 개 만들기

--- src/main/java/com/edu/java/controller/CourseController.java
+++ src/main/java/com/edu/java/controller/CourseController.java
@@ -3,10 +3,6 @@ package com.edu.java.controller;
 import java.util.List;
 import java.util.Map;
 
-import javax.inject.Inject;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,16 +12,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.edu.java.CmmService;
-import com.edu.java.service.AdminService;
-import com.edu.java.service.CourseService;
+import com.edu.java.dao.AdminDao;
+import com.edu.java.dao.CourseDao;
 import com.edu.java.dto.Criteria;
 import com.edu.java.dto.PageDto;
+import com.edu.java.service.AdminService;
+import com.edu.java.service.CourseService;
 
 @Controller
 public class CourseController {
-	private static final Logger logger = LoggerFactory.getLogger(CourseController.class);
 
-	@Inject
+	@Autowired
 	CourseService courseService;
 
 	@Autowired
@@ -33,36 +30,47 @@ public class CourseController {
 	
 	@Autowired
 	CmmService cmmService;
+	
+	@Autowired
+	AdminDao adminDao;
+	
+	@Autowired
+	CourseDao courseDao;
 
-	/*@RequestMapping(value="/courseList", method=RequestMethod.GET)
+	@RequestMapping(value="/getCourse", method=RequestMethod.GET)
 	public ModelAndView courseList(PageDto dto, @ModelAttribute("cri") Criteria cri) throws Exception{
-		logger.info("course LIST PAGE");
 		ModelAndView mav = new ModelAndView("jsonView");
 		dto.setCri(cri); 									// page와 amount 설정
-		dto.setTotal(adminBiz.getTotalCount(dto.getKeyword())); 	// 총 게시글 수 조회하는 로직 담기
+		dto.setTotal(adminDao.getTotalCount(dto.getKeyword())); 	// 총 게시글 수 조회하는 로직 담기
 
-		List<Map<String, Object>> list = adminBiz.courseList(dto, cri);
+		List<Map<String, Object>> list = adminDao.courseList(dto, cri);
 		mav.addObject("list", list);
 		mav.addObject("dto", dto); // total 값 가져오기 => 페이지 수 카운트
 		mav.addObject("cri", cri);
-		mav.setViewName("/edu_Application/courseList");
+		mav.setViewName("/user/application/courseList");
 
 		return mav;
 	}
 
-	@RequestMapping(value="courseDetail", method=RequestMethod.GET)
-	public ModelAndView courseDetail(@RequestParam("c_no") int c_no, @ModelAttribute("cri") Criteria cri) throws Exception{
+	@RequestMapping(value="/coursedetail", method=RequestMethod.GET)
+	public ModelAndView courseDetail() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("/user/application/courseDetail");
+
+		return mav;
+	}
+	
+	@RequestMapping(value="/coureDetail", method=RequestMethod.POST)
+	public ModelAndView courseDetailPost(@RequestParam("seq") int seq, @ModelAttribute("cri") Criteria cri) {
 		ModelAndView mav = new ModelAndView("jsonView");
-
-		mav.addObject("dto", courseBiz.selectOne(c_no));
+		mav.addObject("dto", courseDao.selectOne(seq));
 		mav.addObject("cri", cri);
-		mav.setViewName("/edu_Application/courseDetail");
-
+		
 		return mav;
 	}
 
 	@RequestMapping(value="/appForm", method=RequestMethod.GET)
 	public String appForm() {
-		return "/edu_Application/appForm";
-	}*/
+		return "/user/application/appForm";
+	}
 }
