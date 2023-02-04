715ebb0ce9339545ae2754fb96416864776c4c8c
YEJI <yedoong919@gmail.com>
2023-02-04 23:24:35 +0900

    0204 _ 필요없는 주석 제거

--- src/main/java/com/edu/java/dao/CourseDao.java
+++ src/main/java/com/edu/java/dao/CourseDao.java
@@ -9,16 +9,10 @@ import com.edu.java.dto.PageDto;
 public interface CourseDao {
 	String NAMESPACE = "course.";
 	
-	// 교육 전체 리스트
-	public List<CourseDto> courseList(PageDto dto);
-	
-	// 교육 상세 페이지
-	public CourseDto selectOne(int c_no);
-	
-	// 교육 신청
-	public int courseAppInsert(HashMap<String, Object> params) throws Exception;	
+	List<CourseDto> courseList(PageDto dto);
+	CourseDto selectOne(int c_no);
+	int courseAppInsert(HashMap<String, Object> params) throws Exception;
 	// count로 중복 신청 안되게 하려고 아래 하나 더 작성해보는 것
-	//public HashMap<String, Object> courseAppInsert(HashMap<String, Object> params) throws Exception;
-
-	public int getAppTotal(String param);
+	//HashMap<String, Object> courseAppInsert(HashMap<String, Object> params) throws Exception;
+	int getAppTotal(String param);
 }
--- src/main/java/com/edu/java/dao/UserDaoImpl.java
+++ src/main/java/com/edu/java/dao/UserDaoImpl.java
@@ -14,40 +14,33 @@ public class UserDaoImpl implements UserDao{
 	@Inject
 	SqlSession sqlSession;
 	
-	// 로그인
 	@Override
 	public HashMap<String, Object> loginCheck(HashMap<String, Object> map) { 
 		return sqlSession.selectOne(NAMESPACE + "loginCheck", map); 
 	}
 	
-	// 로그아웃
 	@Override
 	public void logout(UserDto dto) throws Exception {
 		// TODO Auto-generated method stub
 	}
 	
-	// id 중복 체크
 	@Override
 	public int idCheck(String user_id) throws Exception{
 		return sqlSession.selectOne(NAMESPACE+ "idCheck", user_id);
 	}
 
-	// 회원가입
 	@Override
-	public void memberRegist(UserDto dto) throws Exception {
-		sqlSession.insert(NAMESPACE + "memberRegist", dto);
-	}
-	
-	//회원정보 수정
-	@Override
-	public void memberUpdate(UserDto dto) throws Exception {
-		sqlSession.update(NAMESPACE + "memberUpdate", dto);
+	public void inputUser(UserDto dto) throws Exception {
+
 	}
 
-	//회원 비활성화
 	@Override
-	public void memberDelete(UserDto dto) throws Exception {
-		sqlSession.update(NAMESPACE + "memberDelete", dto);
+	public void modifyUser(UserDto dto) throws Exception {
+
 	}
 
+	@Override
+	public void deleteUser(UserDto dto) throws Exception {
+
+	}
 }
