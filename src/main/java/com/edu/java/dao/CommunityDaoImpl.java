cdea7d6d37e462e0586f2752a9d813e70d92cbab
YEJI <yedoong919@gmail.com>
2023-02-06 21:30:06 +0900

    0206_ CommunityDaoImpl noticeDetail 추가

--- src/main/java/com/edu/java/dao/CommunityDaoImpl.java
+++ src/main/java/com/edu/java/dao/CommunityDaoImpl.java
@@ -3,6 +3,7 @@ package com.edu.java.dao;
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -11,31 +12,44 @@ import com.edu.java.dto.FaqDto;
 import com.edu.java.dto.NoticeDto;
 import com.edu.java.dto.QnaDto;
 
+@Slf4j
 @Repository
 public class CommunityDaoImpl implements CommunityDao{
+
 	@Autowired
 	private SqlSessionTemplate sqlSession;
 	
 	@Override
-	public List<NoticeDto> notice(){
-		List<NoticeDto> notice = new ArrayList<>();
+	public List<NoticeDto> noticeList(){
+		List<NoticeDto> noticeList = new ArrayList<>();
 		
 		try {
-			notice = sqlSession.selectList(NAMESPACE + "notice");
+			noticeList = sqlSession.selectList(NAMESPACE + "noticeList");
 		} catch (Exception e) {
 			System.out.println("[error] : notice list");
 			e.printStackTrace();
 		}
-		return notice;
-		
+		return noticeList;
 	}
-	
+
 	@Override
-	public List<QnaDto> qna(){
+	public NoticeDto noticeDetail(int notice_seq) {
+		NoticeDto selectOne = new NoticeDto();
+
+		try {
+			selectOne = sqlSession.selectOne(NAMESPACE + "noticeDetail");
+		}catch (Exception e) {
+			log.debug("[ Error ] notice Detail "+e.getMessage());
+		}
+		return selectOne;
+	}
+
+	@Override
+	public List<QnaDto> qnaList(){
 		List<QnaDto> qna = new ArrayList<>();
 		
 		try {
-			qna = sqlSession.selectList(NAMESPACE + "qna");
+			qna = sqlSession.selectList(NAMESPACE + "qnaList");
 		} catch (Exception e) {
 			System.out.println("[error] : qna list");
 			e.printStackTrace();
@@ -44,11 +58,11 @@ public class CommunityDaoImpl implements CommunityDao{
 	}
 	
 	@Override
-	public List<FaqDto> faq(){
+	public List<FaqDto> faqList(){
 		List<FaqDto> faq = new ArrayList<>();
 		
 		try {
-			faq = sqlSession.selectList(NAMESPACE + "faq");
+			faq = sqlSession.selectList(NAMESPACE + "faqList");
 		} catch (Exception e) {
 			System.out.println("[error] : faq list");
 			e.printStackTrace();
