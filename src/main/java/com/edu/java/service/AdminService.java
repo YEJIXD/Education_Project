11bceed5e9c637feaed732e492410f3a935551db
YJ <yedoong919@gmail.com>
2023-02-19 23:28:54 +0900

    0219_ AdminService implements AdminMapper

--- src/main/java/com/edu/java/service/AdminService.java
+++ src/main/java/com/edu/java/service/AdminService.java
@@ -1,20 +1,21 @@
 package com.edu.java.service;
 
 import java.util.List;
+import java.util.Map;
 
+import com.edu.java.dto.*;
+import com.edu.java.mapper.AdminMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.edu.java.dao.AdminDao;
-import com.edu.java.dto.NoticeDto;
-import com.edu.java.dto.QnaDto;
 
 @Service
-public class AdminService {
+public class AdminService implements AdminMapper {
 	
 	@Autowired
 	AdminDao adminDao;
-	
+
 	public int userCount() {
 		return adminDao.userCount();
 	}
@@ -47,50 +48,110 @@ public class AdminService {
 		return adminDao.selectNotice(seq);
 	}
 	
-	public int inputNotice(NoticeDto dto) {
-		return adminDao.inputNotice(dto);
+	public void inputNotice(NoticeDto dto) {
+		adminDao.inputNotice(dto);
 	}
 	
-	public int modifyNotice(NoticeDto dto) {
-		return adminDao.modifyNotice(dto);
+	public void modifyNotice(NoticeDto dto) {
+		adminDao.modifyNotice(dto);
 	}
 	
-	public int deleteNotice(int seq) {
-		return adminDao.deleteNotice(seq);
+	public void deleteNotice(int seq) {
+		adminDao.deleteNotice(seq);
 	}
 	
-	public List<QnaDto> getQna(){
-		return adminDao.getQna();
+	public List<QnaDto> qnaList(){
+		return adminDao.qnaList();
 	}
+
+	public QnaDto selectQna(int q_no) {
+		return adminDao.selectQna(q_no);
+	}
+
+	public void deleteQna(int q_no){
+		adminDao.deleteQna(q_no);
+	};
 	
-	/*public QnaDto selectQna(int q_no);
-	public int deleteQna(int q_no);
-	
-	public List<FaqDto> getFaq();
-	public FaqDto faqDetail(int faq_no);
-	public int inputFaq(FaqDto dto);
-	public int modifyFaq(FaqDto dto);
-	public int deleteFaq(int faq_no);
-	public List<FaqDto> hashTagSearch(String faq_category);
-	
-	public List<Map<String, Object>> courseList(PageDto dto, Criteria cri) ;
-	public int getTotalCount(String param);
-	//List<Map<String, Object>> selectBoardList(Criteria cri);
-	public CourseDto courseDetail(int c_no) throws Exception;
-	public int inputCourse(CourseDto dto) throws Exception;
-	public int modifyCourse(CourseDto dto) throws Exception;
-	public void deleteCourse(int c_no);
+	public List<FaqDto> faqList(){
+		return adminDao.faqList();
+	}
+
+	public FaqDto selectFaq(int faq_no) {
+		return adminDao.selectFaq(faq_no);
+	}
+
+	public void inputFaq(FaqDto dto){
+		adminDao.inputFaq(dto);
+	}
+
+	public void modifyFaq(FaqDto dto){
+		adminDao.modifyFaq(dto);
+	}
+
+	public void deleteFaq(int faq_no){
+		adminDao.deleteFaq(faq_no);
+	}
+
+	public List<FaqDto> hashTagSearch(String faq_category){
+		return adminDao.hashTagSearch(faq_category);
+	}
+
+	public List<Map<String, Object>> courseList(PageDto dto, Criteria cri){
+		return adminDao.courseList(dto, cri);
+	}
+
+	public int getTotal(String param){
+		return adminDao.getTotal(param);
+	}
+
+	public CourseDto selectCourse(int c_no) {
+		try {
+			return adminDao.selectCourse(c_no);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void inputCourse(CourseDto dto){
+		try {
+			adminDao.inputCourse(dto);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void modifyCourse(CourseDto dto) {
+		try {
+			adminDao.modifyCourse(dto);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void deleteCourse(int c_no){
+		adminDao.deleteCourse(c_no);
+	}
 	//public int courseCount(String c_count) throws Exception;
 	
-	public List<TeacherDto> teacherList();
-	public int inputTeacher(TeacherDto dto);
-	public int modifyTeacher(TeacherDto dto);
-	public int deleteTeacher(int t_no);
+	public List<TeacherDto> teacherList(){
+		return adminDao.teacherList();
+	}
+	public void inputTeacher(TeacherDto dto){
+		adminDao.inputTeacher(dto);
+	}
+	public void modifyTeacher(TeacherDto dto){
+		adminDao.modifyTeacher(dto);
+	}
+
+	public void deleteTeacher(int t_no){
+		adminDao.deleteTeacher(t_no);
+	}
 	
-	public List<MemberDto> memberList();
-	public int deleteMember(int user_no);
-	
-	public List<Map<String, Object>> appList(PageDto dto, Criteria cri);
-	public int getAppTotal(String param);
-	public ApplicationDto appDetail(int app_no) throws Exception;*/
+	public List<UserDto> userList(){
+		return adminDao.userList();
+	}
+
+	public void deleteUser(int user_no){
+		adminDao.deleteUser(user_no);
+	}
 }
